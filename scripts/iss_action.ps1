875e1bfa17d887d9099ae87a6a82220e95abf455
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2022-07-31 09:39:17 +0700

    Add Remote server

--- scripts/iss_action.ps1
+++ scripts/iss_action.ps1
@@ -1,10 +1,16 @@
 Param(
     [parameter(Mandatory = $true)]
+    [string]$server,
+    [parameter(Mandatory = $true)]
     [string]$app_name,
     [parameter(Mandatory = $true)]
     [string]$app_pool_name,
     [parameter(Mandatory = $true)]
     [string]$physical_path,
+    [parameter(Mandatory = $true)]
+    [string]$deploy_user_id,
+    [parameter(Mandatory = $true)]
+    [SecureString]$deploy_user_secret
     [string]$website_name = "",
     [string]$user_service = "",
     [string]$password_service = ""
@@ -13,56 +19,73 @@ Param(
 # Import module for creating webapp on IIS
 Import-Module WebAdministration;
 
-# set Default value
-if ($physical_path.ToString() -eq "")
-{
-    $physical_path = "C:\inetpub\wwwroot\";
+$credential = [PSCredential]::new($deploy_user_id, $deploy_user_secret)
+$so = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
+
+$app_pool_credential = [PSCredential]::new($app_pool_user_id, $app_pool_user_secret)
+$set_app_pool_secret = $app_pool_credential.GetNetworkCredential().Password
+
+
+$script = {
+    # set Default value
+    if ($physical_path.ToString() -eq "")
+    {
+        $physical_path = "C:\inetpub\wwwroot\";
+    }
+
+    # set Default value
+    if ($website_name.ToString() -eq "")
+    {
+        $website_name = "Default Web Site";
+    }
+
+    # create app pool if it doesn't exist
+    if (Get-IISAppPool -Name $app_pool_name)
+    {
+        Write-Output "The App Pool $app_pool_name already exists"
+    }
+    else
+    {
+        Write-Output "Creating app pool $app_pool_name"
+        $app_pool = New-WebAppPool -Name $app_pool_name
+        $app_pool.autoStart = $true
+        $app_pool.managedPipelineMode = "Integrated"
+        $app_pool | Set-Item
+        Write-Output "App pool $app_pool_name has been created"
+    }
+
+    # create the folder if it doesn't exist
+    if (Test-path $physical_path)
+    {
+        Write-Output "The folder $physical_path already exists"
+    }
+    else
+    {
+        New-Item -ItemType Directory -Path $physical_path -Force
+        Write-Output "Created folder $physical_path"
+    }
+
+    # Run as the user(set service account)
+    if (($user_service.ToString() -eq "") -or ($password_service.ToString() -eq ""))
+    {
+        Write-Output "Do not set property for $app_pool_name"
+    }
+    else
+    {
+        Write-Output "Set property for $app_pool_name"
+        Set-ItemProperty IIS:\AppPools\$app_pool_name -name processModel -value @{userName=$user_service;password=$set_app_pool_secret;identitytype=3}
+    }
+
+    # Create New WebApplication
+    New-WebApplication "$app_name" -Site "$website_name" -ApplicationPool "$app_pool_name"  -PhysicalPath "$physical_path" -Force;
+
+    Write-Output "Deploy WebApp sucessfully"
 }
 
-# set Default value
-if ($website_name.ToString() -eq "")
-{
-    $website_name = "Default Web Site";
-}
+Invoke-Command -ComputerName $server `
+    -Credential $credential `
+    -UseSSL `
+    -SessionOption $so `
+    -ScriptBlock $script
 
-# create app pool if it doesn't exist
-if (Get-IISAppPool -Name $app_pool_name)
-{
-    Write-Output "The App Pool $app_pool_name already exists"
-}
-else
-{
-    Write-Output "Creating app pool $app_pool_name"
-    $app_pool = New-WebAppPool -Name $app_pool_name
-    $app_pool.autoStart = $true
-    $app_pool.managedPipelineMode = "Integrated"
-    $app_pool | Set-Item
-    Write-Output "App pool $app_pool_name has been created"
-}
-
- # create the folder if it doesn't exist
-if (Test-path $physical_path)
-{
-    Write-Output "The folder $physical_path already exists"
-}
-else
-{
-    New-Item -ItemType Directory -Path $physical_path -Force
-    Write-Output "Created folder $physical_path"
-}
-
-# Run as the user(set service account)
-if (($user_service.ToString() -eq "") -or ($password_service.ToString() -eq ""))
-{
-    Write-Output "Do not set property for $app_pool_name"
-}
-else
-{
-    Write-Output "Set property for $app_pool_name"
-    Set-ItemProperty IIS:\AppPools\$app_pool_name -name processModel -value @{userName=$user_service;password=$password_service;identitytype=3}
-}
-
-# Create New WebApplication
-New-WebApplication "$app_name" -Site "$website_name" -ApplicationPool "$app_pool_name"  -PhysicalPath "$physical_path" -Force;
-
-Write-Output "Deploy WebApp sucessfully"
+Write-Output "IIS Site Created"
